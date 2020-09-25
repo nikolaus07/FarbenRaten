a4a4a6f06f4250ad9db763ca4be1ffd84aad1283
Klaus Thevessen <flm@thevessen.ch>
2020-09-25 16:42:06 +0200

    1. entwurf läuft

--- FarbenRaten/Form1.Designer.cs
+++ FarbenRaten/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(223, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Farben anzeigen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(27, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ende";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(27, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 102);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hiermit kann man die Intuition üben.\r\nErst sich eine Farbe ausdenken:\r\nRot, Grün," +
+    " Grau, Gelb, Blau oder Orange.\r\n\r\nDann click \'Farbe anzeigen\'\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 251);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Farbenrate spiel zum trainieren der Intuition 09.2020";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- FarbenRaten/Form1.cs
+++ FarbenRaten/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FarbenRaten
@@ -15,6 +9,45 @@ namespace FarbenRaten
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Button farben anzeigen
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random _random = new Random();
+            int nummer = _random.Next(1, 7);
+            string xx = "rot.png";
+
+            switch (nummer)
+            {
+                case 1: xx = "gelb.png"; break;
+                case 2: xx = "grau.png"; break;
+                case 3: xx = "gruen.png"; break;
+                case 4: xx = "orange.png"; break;
+                case 5: xx = "rot.png"; break;
+                case 6: xx = "blau.png"; break;
+                default: xx = "gelb.png"; break;
+            }
+
+            pictureBox1.Load(xx);
+
+
+        }
+
+        // ende button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
